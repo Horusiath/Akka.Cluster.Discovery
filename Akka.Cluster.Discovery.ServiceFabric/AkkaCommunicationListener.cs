b8cbeb449405933e0e9711b35f307cf34a44a035
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2017-07-04 21:07:11 +0200

    Service.Fabric: init commit

---
+++ Akka.Cluster.Discovery.ServiceFabric/AkkaCommunicationListener.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+
+namespace Akka.Cluster.Discovery.ServiceFabric
+{
+    public class AkkaCommunicationListener : ICommunicationListener
+    {
+        private readonly ActorSystem system;
+        private readonly Akka.Cluster.Cluster cluster;
+
+        public AkkaCommunicationListener(ActorSystem system)
+        {
+            this.system = system;
+            this.cluster = Cluster.Get(system);
+        }
+
+        public Task<string> OpenAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            return cluster.LeaveAsync(cancellationToken);
+        }
+
+        public void Abort()
+        {
+            cluster.LeaveAsync().Wait();
+        }
+    }
+}
---
+++ Akka.Cluster.Discovery.ServiceFabric/ServiceFabricDiscoveryService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace Akka.Cluster.Discovery.ServiceFabric
+{
+    public sealed class ServiceFabricDiscoveryService: LocklessDiscoveryService
+    {
+        public ServiceFabricDiscoveryService(Config config) : this(new ServiceFabricSettings(config))
+        {
+        }
+
+        public ServiceFabricDiscoveryService(ServiceFabricSettings settings) : base(settings)
+        {
+        }
+
+        protected override Task<IEnumerable<Address>> GetAliveNodesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override Task RegisterNodeAsync(MemberEntry node)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override Task MarkAsAliveAsync(MemberEntry node)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
